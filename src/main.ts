df4a6997dbb11674e45acaf6e35579733541e6b5
YourAKShaw <hitme@yourakshaw.com>
2024-07-17 16:41:46 +0530

    feat(mongo): :card_file_box: initiate db connection in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config(); // Load environment variables from .env file
 
 import app from "./app";
 import logger from "./common/logger";
+import { connectToDb } from "./common/mongoClient";
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   logger.info(`Server started on port ${PORT}`);
 });
+
+connectToDb();
