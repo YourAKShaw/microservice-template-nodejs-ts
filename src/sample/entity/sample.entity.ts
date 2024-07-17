aeeea8338aca1d641ba7b51dd63959f96c8f21b2
YourAKShaw <hitme@yourakshaw.com>
2024-07-17 20:40:32 +0530

    feat(sample): :card_file_box: add sample.entity.ts

---
+++ src/sample/entity/sample.entity.ts
@@ -0,0 +1,15 @@
+export default class Sample {
+  sampleBoolean: boolean;
+  sampleNumber: number;
+  sampleString: string;
+
+  constructor(
+    sampleBoolean: boolean,
+    sampleNumber: number,
+    sampleString: string,
+  ) {
+    this.sampleBoolean = sampleBoolean;
+    this.sampleNumber = sampleNumber;
+    this.sampleString = sampleString;
+  }
+}
