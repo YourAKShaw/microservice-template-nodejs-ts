9305c87c4700ccab5d034fb86192b09bd6eb68d0
YourAKShaw <hitme@yourakshaw.com>
2024-07-17 17:07:51 +0530

    style(log): :art: add printAsciiArt.ts and log it at start of server

---
+++ src/common/helpers/printAsciiArt.ts
@@ -0,0 +1,13 @@
+function printAsciiArt() {
+  console.log();
+  const repoAsciiArt = String.raw`
+   __ _   __  ____  ____       __  ____     _     ____  ____ 
+  (  ( \ /  \(    \(  __)    _(  )/ ___)   ( )   (_  _)/ ___)
+  /    /(  O )) D ( ) _)  _ / \) \\___ \  (_ _)    )(  \___ \
+  \_)__) \__/(____/(____)(_)\____/(____/   (_)    (__) (____/  
+  `;
+  console.log(repoAsciiArt);
+  console.log();
+}
+
+export default printAsciiArt;
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,11 @@ dotenv.config(); // Load environment variables from .env file
 import app from "./app";
 import logger from "./common/logger";
 import { connectToDb } from "./common/mongoClient";
+import printAsciiArt from "./common/helpers/printAsciiArt";
 
 const PORT = process.env.PORT || 3000;
+
+printAsciiArt();
 
 app.listen(PORT, () => {
   logger.info(`Server started on port ${PORT}`);
